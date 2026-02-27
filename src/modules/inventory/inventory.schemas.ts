7955fa804941e437092c2a91a71db711040d7840
zer4c <pabloazurduy@gmail.com>
2026-02-27 14:40:45 -0400

    doc: add schema documentation for inventory module

--- src/modules/inventory/inventory.schemas.ts
+++ src/modules/inventory/inventory.schemas.ts
@@ -1,19 +1,33 @@
 import { z } from 'zod';
 
 const InventoryBase = z.strictObject({
-  stock: z.int().min(0),
+  stock: z.int().min(0).openapi({
+    example: 0,
+  }),
 });
 
-export const InventoryCreate = InventoryBase.extend({});
-export const InventoryResponse = z.object({
-  ...InventoryCreate.shape,
-  id: z.int(),
-  productId: z.int().min(0),
-});
+export const InventoryCreate = InventoryBase.extend({}).openapi(
+  'InventoryCreate',
+);
+export const InventoryResponse = z
+  .object({
+    ...InventoryCreate.shape,
+    id: z.int().openapi({
+      example: 1,
+    }),
+    productId: z.int().min(0).openapi({
+      example: 1,
+    }),
+  })
+  .openapi('InventoryResponse');
 
-export const InventoryPatch = z.object({
-  stock: z.int().min(1),
-});
+export const InventoryPatch = z
+  .object({
+    stock: z.int().min(1).openapi({
+      example: 1,
+    }),
+  })
+  .openapi('InventoryPatch');
 
 export type InventoryCreateType = z.infer<typeof InventoryCreate>;
 export type InventoryResponseType = z.infer<typeof InventoryResponse>;
